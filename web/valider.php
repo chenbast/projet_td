50d2532799f32c95bf9a6c9651654f136850dc73
Bastien Cheneval-Pallud <bastien.cheneval-pallud@etu.univ-smb.fr>
2026-04-17 09:30:34 +0200

    correction syntaxe sql pour modif dans la base de données

--- web/valider.php
+++ web/valider.php
@@ -10,16 +10,14 @@
         $new_date=$_POST['date'];
         $new_heure=$_POST['heure'];
         $numDetec = $_GET['id'];
-        echo $new_date;
-        echo $new_heure;
-        echo $numDetec;
         $dbh = new PDO('mysql:dbname=projet_rfid;host=localhost;charset=utf8', 'root', '');
-        $sql="UPDATE detection SET date = $new_date, heure = $new_heure WHERE num_detec = $numDetec";
+        $sql = "UPDATE detection SET `date` = '$new_date', heure = '$new_heure' WHERE num_detec = $numDetec";
         $result = $dbh->query($sql);
-
-          echo 'La détection n°'.$numDetec.' a bien été changé avec la date '.$new_date.' et avec la date '.$new_heure.'.';    
-        ?>       
-
+        echo 'La détection n°'.$numDetec.' a bien été changé avec la date '.$new_date.' et avec la date '.$new_heure.'.';    
+        
+        ?> 
+      <img height=80 src="images/fleche_retour.png">
+      <a href="espace_admin.php">Retour</a>      
     </div>
     </body>
     <script src="projet.js"></script>
